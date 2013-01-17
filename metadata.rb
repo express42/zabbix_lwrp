9a397972f36454a659d4b61118b7516ba52e3a7c
Alexander Titov <titoff.a@gmail.com>
2013-01-17 17:46:16 +0400

    add initial file structure

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'zabbix-client'
+maintainer       'Express 42 LLC'
+maintainer_email 'cookbooks@express42.com'
+license          'Apache 2.0'
+description      'Installs/Configures zabbix-client'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
