b15f771ef282efa16af5b247888fa5d4338d6250
Ivan Evtukhovich <evtuhovich@gmail.com>
2013-05-05 21:06:52 +0400

    Add more logs for debug

--- providers/application.rb
+++ providers/application.rb
@@ -65,8 +65,8 @@ action :sync do
 
       # FIXME: update existing trigger
     else
-      Chef::Log.info "#{trigger} should be created"
-      converge_by("Create #{trigger.to_hash}") do
+      Chef::Log.info "#{trigger.to_hash} should be created"
+      converge_by("Create #{trigger}") do
         Rubix::Trigger.new(trigger.to_hash).save!
       end
     end
