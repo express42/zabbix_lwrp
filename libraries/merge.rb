d1db33c61ba4971fa5e619280114c13564eccaba
Nikita Borzykh <sample.n@gmail.com>
2014-05-30 18:43:56 +0400

    Replace normal attributes to default

--- libraries/merge.rb
+++ libraries/merge.rb
@@ -2,12 +2,12 @@ def add_data(node, fqdn, hash)
   dm = Chef::Mixin::DeepMerge
 
   if node['zabbix'] && node['zabbix']['hosts'] && node['zabbix']['hosts'][fqdn]
-    existing_data = node.normal['zabbix']['hosts'][fqdn]
+    existing_data = node.default['zabbix']['hosts'][fqdn]
   else
     existing_data = {}
   end
 
   result = dm.merge existing_data, hash
 
-  node.normal['zabbix']['hosts'][fqdn] = result
+  node.default['zabbix']['hosts'][fqdn] = result
 end
