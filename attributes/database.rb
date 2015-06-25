02f29eaab8c8166ed5558e99aab9291e33eabb2c
Igor Kurochkin <ikurochkin@express42.com>
2015-06-25 14:09:30 +0300

    Add partition attributes

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,12 +1,14 @@
+default['zabbix']['server']['database']['filesystem'] = 'ext4'
 default['zabbix']['server']['database']['lvm_group'] = 'shared'
+default['zabbix']['server']['database']['lvm_volume'] = '/dev/sda3'
 default['zabbix']['server']['database']['partition_size'] = '10G'
 
-default['zabbix']['server']['database']['databag'] = 'zabbix'
-default['zabbix']['server']['database']['network'] = '127.0.0.0/8'
-default['zabbix']['server']['database']['version'] = '9.4'
 default['zabbix']['server']['database']['cluster'] = 'main'
+default['zabbix']['server']['database']['databag'] = 'zabbix'
 default['zabbix']['server']['database']['locale'] = 'en_US.utf8'
 default['zabbix']['server']['database']['mount_point'] = '/var/lib/postgresql'
+default['zabbix']['server']['database']['network'] = '127.0.0.0/8'
+default['zabbix']['server']['database']['version'] = '9.4'
 
 default['zabbix']['server']['database']['configuration']['listen_addresses'] = '127.0.0.1'
 default['zabbix']['server']['database']['configuration']['port'] = '5432'
