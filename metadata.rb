5a3c3dedd4336a90241f50b3a18c10c93511b80b
Igor Kurochkin <ikurochkin@express42.com>
2015-06-25 16:06:22 +0300

    Version bump to 1.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'cookbooks@express42.com'
 license          'MIT'
 description      'Installs and configures Zabbix agent and server with PostgreSQL and Nginx. Provides LWRP for creating and modifying Zabbix objects.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.2'
+version          '1.1.3'
 
 depends          'apt'
 depends          'build-essential'
 depends          'lvm'
 depends          'nginx'
 depends          'postgresql_lwrp'
-depends          'php'
+depends          'php-fpm'
 
 recipe           'zabbix_lwrp::agent', 'Installs and configures Zabbix agent.'
 recipe           'zabbix_lwrp::connect', 'Connects to Zabbix API to sync configuration.'
