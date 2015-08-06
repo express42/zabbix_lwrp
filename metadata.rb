cbe5e0c7209773b564e68d40ae69b4fefc8e19ed
Igor Kurochkin <ikurochkin@express42.com>
2015-08-06 18:09:05 +0300

    Version bump to 1.1.11

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@express42.com'
 license          'MIT'
 description      'Installs and configures Zabbix agent and server with PostgreSQL and Nginx. Provides LWRP for creating and modifying Zabbix objects.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.10'
+version          '1.1.11'
 
 depends          'apt'
 depends          'build-essential'
