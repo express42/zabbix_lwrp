3f469b01a865ffdd231e9f8e20e07431a033fedf
AlmZ <rukikru@gmail.com>
2017-05-12 11:27:40 +0300

    Modify database recipe

--- recipes/database.rb
+++ recipes/database.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: zabbix_lwrp
 # Recipe:: database
 #
-# Copyright (C) LLC 2017 Express 42
+# Copyright (C) LLC 2015-2017 Express 42
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
@@ -21,6 +21,28 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+case node['platform_family']
+when 'rhel'
+  pg_rhel_version = node['postgresql']['version'].gsub(/./, '')
+  node.default['postgresql']['enable_pgdg_yum'] = true
+  node.default['postgresql']['use_pgdg_packages'] = true
+  node.default['postgresql']['client']['packages'] = "postgresql#{pg_rhel_version}-devel"
+  node.default['postgresql']['server']['packages'] = ["postgresql#{pg_rhel_version}-server"]
+  node.default['postgresql']['contrib']['packages'] = ["postgresql#{pg_rhel_version}-contrib"]
+  node.default['postgresql']['setup_script'] = "postgresql#{pg_rhel_version}-setup"
+  node.default['postgresql']['server']['service_name'] = "postgresql-#{node['postgresql']['version']}"
+when 'debian'
+  node.default['postgresql']['enable_pgdg_apt'] = true
+  node.default['postgresql']['use_pgdg_packages'] = true
+  node.default['postgresql']['dir'] = "/etc/postgresql/#{node['postgresql']['version']}/main"
+  node.default['postgresql']['client']['packages'] = ["postgresql-client-#{node['postgresql']['version']}", 'libpq-dev']
+  node.default['postgresql']['server']['packages'] = ["postgresql-#{node['postgresql']['version']}"]
+  node.default['postgresql']['contrib']['packages'] = ["postgresql-contrib-#{node['postgresql']['version']}"]
+  node.default['postgresql']['server']['service_name'] = 'postgresql'
+end
+
+include_recipe 'postgresql::server'
+include_recipe 'postgresql::ruby'
 
 if node['zabbix']['server']['database']['databag'].nil? ||
    node['zabbix']['server']['database']['databag'].empty? ||
