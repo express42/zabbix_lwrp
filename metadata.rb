654e2b94641382a8f454bf5bd6054a38404d5b08
Igor Kurochkin <ikurochkin@express42.com>
2017-05-16 18:14:43 +0300

    Fix chef_version again

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'MIT'
 description      'Installs and configures Zabbix agent and server with PostgreSQL and Nginx. Provides LWRP for creating and modifying Zabbix objects.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.1'
+chef_version     '>= 12.5' if respond_to?(:chef_version)
 source_url       'https://github.com/express42/zabbix_lwrp' if respond_to?(:source_url)
 issues_url       'https://github.com/express42/zabbix_lwrp/issues' if respond_to?(:issues_url)
 
