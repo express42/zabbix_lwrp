bc570bfbd7f4e963b59564e548ffdf76dd36a747
AlmZ <rukikru@gmail.com>
2017-05-29 15:03:24 +0300

    Update pg version in tests

--- test/fixtures/cookbooks/zabbix_lwrp_test/recipes/zabbix24_postgres_without_lvm.rb
+++ test/fixtures/cookbooks/zabbix_lwrp_test/recipes/zabbix24_postgres_without_lvm.rb
@@ -1,9 +1,9 @@
 include_recipe 'apt'
 include_recipe 'chef_nginx::default'
 
+node.default['zabbix']['server']['database']['postgresql']['version'] = '9.6'
 # Temporary use higher version of zabbixapi, for correct tests works
 # In gem zabbixapi==2.4.X uses old json gem (==1.6.1) but in chefdk uses newest version
-node.default['zabbix']['server']['database']['version'] = '9.6'
 node.default['zabbix']['version'] = '2.4'
 node.default['zabbix']['api-version'] = '3.0.0'
 
