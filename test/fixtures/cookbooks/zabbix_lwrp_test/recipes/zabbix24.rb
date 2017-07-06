ea61994ce26c64db10bb257365b7134da498ebf2
chromko <mich.dima@gmail.com>
2017-07-06 13:55:30 +0300

    Fix error when ipaddress atribute is empty

--- test/fixtures/cookbooks/zabbix_lwrp_test/recipes/zabbix24.rb
+++ test/fixtures/cookbooks/zabbix_lwrp_test/recipes/zabbix24.rb
@@ -46,6 +46,8 @@ include_recipe 'chef_nginx::default'
 node.default['zabbix']['server']['database']['version'] = '9.6'
 node.default['zabbix']['version'] = '2.4'
 node.default['zabbix']['api-version'] = '3.0.0'
+node.default['zabbix']['host']['agent']['use_ip'] = false if node['zabbix']['host']['ipaddress'].to_s.empty?
+node.default['zabbix']['host']['agent']['ipaddress'] = '' if node['zabbix']['host']['ipaddress'].to_s.empty?
 
 # Temporary use higher version of zabbixapi, for correct tests works
 # In gem zabbixapi==2.4.X uses old json gem (==1.6.1) but in chefdk uses newest version
