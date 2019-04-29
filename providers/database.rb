f5e78f87b72cf70048343a86fb40ac5a58b253db
Jimmy Provencher <jimmy.provencher@cgi.com>
2019-04-29 16:12:18 -0400

    Updating database resource to use zabbix 4.2

--- providers/database.rb
+++ providers/database.rb
@@ -88,10 +88,10 @@ action :create do
   if db_vendor == 'postgresql'
     db_connect_string = "PGPASSWORD=#{db_pass} psql -q -t -h #{db_host} -p #{db_port} -U #{db_user} -d #{db_name}"
 
-    if node['zabbix']['version'].to_f.between?(3.0, 4.0) && node['platform_family'] == 'rhel'
+    if node['zabbix']['version'].to_f.between?(3.0, 4.2) && node['platform_family'] == 'rhel'
       db_command = "gunzip -c /usr/share/doc/zabbix-server-pgsql*/create.sql.gz | #{db_connect_string}"
 
-    elsif node['zabbix']['version'].to_f.between?(3.0, 4.0) && node['platform_family'] == 'debian'
+    elsif node['zabbix']['version'].to_f.between?(3.0, 4.2) && node['platform_family'] == 'debian'
       db_command = "gunzip -c /usr/share/doc/zabbix-server-pgsql/create.sql.gz | #{db_connect_string}"
 
     elsif node['zabbix']['version'].to_f < 3.0 && node['platform_family'] == 'rhel'
@@ -107,10 +107,10 @@ action :create do
   elsif db_vendor == 'mysql'
     db_connect_string = "mysql -h #{db_host} -P #{db_port} -u #{db_user} -p#{db_pass} -D #{db_name}"
 
-    if node['zabbix']['version'].to_f.between?(3.0, 4.0) && node['platform_family'] == 'rhel'
+    if node['zabbix']['version'].to_f.between?(3.0, 4.2) && node['platform_family'] == 'rhel'
       db_command = "zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | #{db_connect_string}"
 
-    elsif node['zabbix']['version'].to_f.between?(3.0, 4.0) && node['platform_family'] == 'debian'
+    elsif node['zabbix']['version'].to_f.between?(3.0, 4.2) && node['platform_family'] == 'debian'
       db_command = "zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | #{db_connect_string}"
 
     elsif node['zabbix']['version'].to_f < 3.0 && node['platform_family'] == 'rhel'
